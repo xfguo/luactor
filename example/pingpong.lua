d8756bd8f84034a1914fe6bd007c110f97cbd66c
Xiongfei(Alex) Guo <xfguo@credosemi.com>
2014-08-04 23:23:23 +0800

    Modify ping-pong example for reproducing #4 (a starvation issue).
    Signed-off-by: Xiongfei(Alex) Guo <xfguo@credosemi.com>

--- example/pingpong.lua
+++ example/pingpong.lua
@@ -4,25 +4,42 @@ local reactor_name = os.getenv("LUACTOR_REACTOR") or 'luaevent'
 print('The reactor you use is *'..reactor_name..'*.')
 
 local ping = function ()
+    -- ping-pong for 1 second
+    actor.register_event(
+        'timeout',      -- event type
+        'stop',           -- event name
+        1               -- event parameters
+    )
     print("ping start")
-    for _ = 1,100 do
+    local finish = false
+    while not finish do
         actor.wait({
             bar = function (msg, sender)
                 print(string.format('msg from:%s msg:%s', sender, msg))
                 actor.send("pong", "foo", "hello")
             end,
+            timeout = function (msg, sender)
+                actor.send("pong", "exit")
+                finish = true
+                print("End")
+            end
         })
     end
 end
 
 local pong = function ()
     print("pong start")
-    for _ = 1,100 do
+    local finish = false
+    while not finish do
         actor.wait({
             foo = function (msg, sender)
                 print(string.format('msg from:%s msg:%s', sender, msg))
                 actor.send("ping", "bar", "world")
             end,
+            timeout = function (msg, sender)
+                finish = true
+                print("End Received")
+            end
         })
     end
 end
